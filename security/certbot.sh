d936ce18e9c0ef8aceeb5e9902df5e7f763b6067
Purnell Jones <plj.coding@gmail.com>
2024-11-09 08:09:22 -0500

    Minor refactor (#2)
    * Rework Github workflow to deploy changed directories. Refactor certbot.sh
    
    * Remove merge conflict.
    
    * Fix workflow.
    
    * Remove merge conflict.

--- security/certbot.sh
+++ security/certbot.sh
@@ -17,47 +17,75 @@ if [ -e $ENV_CONFIG ]; then
     . $ENV_CONFIG
     set +o allexport
 
-    # Check for required variables
-    REQUIRED_VARS="DOMAIN EMAIL API_SUBDOMAIN PORTAINER_SUBDOMAIN"
-    for VAR in $REQUIRED_VARS; do
-        if [ -z "$(eval echo \$$VAR)" ]; then
-            echo "Error: $VAR is not set in $ENV_CONFIG"
-            exit 1
+        # Check for required variables
+        REQUIRED_VARS="DOMAIN EMAIL API_SUBDOMAIN PORTAINER_SUBDOMAIN CA_SIGN_CERTIFICATE_NAME"
+        for VAR in $REQUIRED_VARS; do
+            if [ -z "$(eval echo \$$VAR)" ]; then
+                log_message "ERROR" "Error: $VAR is not set in $ENV_CONFIG"
+                exit 1
+            fi
+        done
+
+        log_message "INFO" "All required variables are set."
+    else
+        log_message "ERROR" "No $ENV_CONFIG found."
+        exit 1
+    fi
+}
+
+
+# set_ssl_path() {
+#     log_message "INFO" "Checking if this machine matches the production DOMAIN: $DOMAIN"
+#     CURRENT_HOSTNAME=$(hostname)
+
+#     if [ "$CURRENT_HOSTNAME" != "$DOMAIN" ]; then
+#         log_message "INFO" "This machine does not match the Production DOMAIN. Expected: $DOMAIN, Found: $CURRENT_HOSTNAME. Defaulting to local domain: $LOCAL_DOMAIN"
+#         IS_LOCAL=1
+#         DOMAIN=$LOCAL_DOMAIN
+#     fi
+
+#     SSL_PATH="/etc/letsencrypt/live/$DOMAIN"
+#     log_message "INFO" "Setting SSL_PATH to $SSL_PATH ..."
+# }
+
+# create_self_signed_certificate() {
+#     log_message "INFO" "Creating self-signed certificate for localhost in $SSL_PATH/$SELF_SIGN_CERTIFICATE_KEY_NAME"
+#     mkdir -p "$SSL_PATH" || { log_message "ERROR" "Could not create directory $SSL_PATH"; exit 1; }
+#     openssl req -newkey rsa:2048 -nodes -keyout ${SSL_PATH}/${SELF_SIGN_CERTIFICATE_KEY_NAME} -x509 -days 365 -out ${SSL_PATH}/${SELF_SIGN_CERTIFICATE_NAME} -subj "/C=US/ST=Florida/L=Orlando/O=Me Dot Com/OU=IT/CN=localhost" || { log_message "ERROR" "Failed to create self-signed certificate"; exit 1; }
+# }
+
+create_certificate_authority_certificate() {
+    log_message "INFO" "Requesting certificate for $DOMAIN and additional subdomains"
+    if [ ! -d "$SSL_PATH" ] || \ 
+        ! openssl x509 -checkend 2592000 -noout -in "$SSL_PATH/fullchain.pem" || \
+        ! is_domain_in_ca_certificate $DOMAIN || \
+        ! is_domain_in_ca_certificate "$API_SUBDOMAIN.$DOMAIN" || \
+        ! is_domain_in_ca_certificate "$PORTAINER_SUBDOMAIN.$DOMAIN"; then
+
+        # Request new or expand existing certificate
+        certbot certonly --webroot --webroot-path=$WEBROOT_PATH --email $EMAIL --agree-tos --no-eff-email --expand -d $DOMAIN -d $API_SUBDOMAIN -d $PORTAINER_SUBDOMAIN
+
+        # Suspend container on failure
+        if [ $? -ne 0 ]; then
+            log_message "WARN" "Failed to obtain certificate for DOMAIN: $DOMAIN API_SUBDOMAIN: $API_SUBDOMAIN PORTAINER_SUBDOMAIN: $PORTAINER_SUBDOMAIN. Entering infinite sleep."
+            while :; do
+                sleep 1d
+            done
         fi
     done
 
     LETSENCRYPT_PATH="/etc/letsencrypt/live/$DOMAIN"
 
-    echo "All required variables are set."
-else
-    echo "No $ENV_CONFIG found."
-    exit 1
-fi
+create_certificate_authority_certificate
 
-echo "Starting Certbot script..."
+# set_ssl_path
 
-# Check if the certificate is about to expire or if there are new domains
-if [ ! -d "$LETSENCRYPT_PATH" ] || \ 
-    ! openssl x509 -checkend 2592000 -noout -in "$LETSENCRYPT_PATH/fullchain.pem" || \
-    ! domain_in_certificate $DOMAIN || \
-    ! domain_in_certificate $API_SUBDOMAIN || \
-    ! domain_in_certificate $PORTAINER_SUBDOMAIN; then
-    
-    echo "Requesting certificate for $DOMAIN and additional subdomains"
-
-    # Request new or expand existing certificate
-    certbot certonly --webroot --webroot-path=$WEBROOT_PATH --email $EMAIL --agree-tos --no-eff-email --expand -d $DOMAIN -d $API_SUBDOMAIN -d $PORTAINER_SUBDOMAIN
-
-    # Suspend container on failure
-    if [ $? -ne 0 ]; then
-        echo "Failed to obtain certificate for DOMAIN: $DOMAIN API_SUBDOMAIN: $API_SUBDOMAIN PORTAINER_SUBDOMAIN: $PORTAINER_SUBDOMAIN. Entering infinite sleep."
-        while :; do
-            sleep 1d
-        done
-    fi
-else
-    echo "Certificate for domains are already valid and not expiring soon."
-fi
+# log_message "INFO" "Creating certificate..."
+# if [ $IS_LOCAL -eq 0 ]; then
+#     create_certificate_authority_certificate
+# else
+#     create_self_signed_certificate
+# fi
 
 while :; do
     echo "Attempting to renew Certbot certification ..."
