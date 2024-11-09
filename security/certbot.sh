93983b591fb48b575b11a6709e88177b7d62128c
jonepl <plj.coding@gmail.com>
2024-11-09 08:07:31 -0500

    Remove merge conflict.

--- security/certbot.sh
+++ security/certbot.sh
@@ -17,14 +17,6 @@ if [ -e $ENV_CONFIG ]; then
     . $ENV_CONFIG
     set +o allexport
 
-<<<<<<< Updated upstream
-    # Check for required variables
-    REQUIRED_VARS="DOMAIN EMAIL API_SUBDOMAIN PORTAINER_SUBDOMAIN"
-    for VAR in $REQUIRED_VARS; do
-        if [ -z "$(eval echo \$$VAR)" ]; then
-            echo "Error: $VAR is not set in $ENV_CONFIG"
-            exit 1
-=======
         # Check for required variables
         REQUIRED_VARS="DOMAIN EMAIL API_SUBDOMAIN PORTAINER_SUBDOMAIN CA_SIGN_CERTIFICATE_NAME"
         for VAR in $REQUIRED_VARS; do
@@ -79,44 +71,11 @@ create_certificate_authority_certificate() {
             while :; do
                 sleep 1d
             done
->>>>>>> Stashed changes
         fi
     done
 
     LETSENCRYPT_PATH="/etc/letsencrypt/live/$DOMAIN"
 
-<<<<<<< Updated upstream
-    echo "All required variables are set."
-else
-    echo "No $ENV_CONFIG found."
-    exit 1
-fi
-
-echo "Starting Certbot script..."
-
-# Check if the certificate is about to expire or if there are new domains
-if [ ! -d "$LETSENCRYPT_PATH" ] || \ 
-    ! openssl x509 -checkend 2592000 -noout -in "$LETSENCRYPT_PATH/fullchain.pem" || \
-    ! domain_in_certificate $DOMAIN || \
-    ! domain_in_certificate $API_SUBDOMAIN || \
-    ! domain_in_certificate $PORTAINER_SUBDOMAIN; then
-    
-    echo "Requesting certificate for $DOMAIN and additional subdomains"
-
-    # Request new or expand existing certificate
-    certbot certonly --webroot --webroot-path=$WEBROOT_PATH --email $EMAIL --agree-tos --no-eff-email --expand -d $DOMAIN -d $API_SUBDOMAIN -d $PORTAINER_SUBDOMAIN
-
-    # Suspend container on failure
-    if [ $? -ne 0 ]; then
-        echo "Failed to obtain certificate for DOMAIN: $DOMAIN API_SUBDOMAIN: $API_SUBDOMAIN PORTAINER_SUBDOMAIN: $PORTAINER_SUBDOMAIN. Entering infinite sleep."
-        while :; do
-            sleep 1d
-        done
-    fi
-else
-    echo "Certificate for domains are already valid and not expiring soon."
-fi
-=======
 create_certificate_authority_certificate
 
 # set_ssl_path
@@ -127,7 +86,6 @@ create_certificate_authority_certificate
 # else
 #     create_self_signed_certificate
 # fi
->>>>>>> Stashed changes
 
 while :; do
     echo "Attempting to renew Certbot certification ..."
