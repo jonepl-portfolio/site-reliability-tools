8e4e8c6aaa87907f02b84a94581188ce0fdd9706
jonepl <plj.coding@gmail.com>
2024-11-14 19:59:38 -0500

    Update entrypoint script.

--- security/certbot.sh
+++ security/certbot.sh
@@ -77,7 +77,7 @@ create_certificate_authority_certificate() {
 renew_certificates_periodically() {
     while :; do
         log_message "INFO" "Attempting to renew Certbot certificates with deploy-hook to update $SWARM_SERVICE_NAME ..."
-        certbot renew --deploy-hook "/usr/bin/docker service update --force $SWARM_SERVICE_NAME"
+        certbot renew --deploy-hook "/usr/bin/docker service update --force $SWARM_SERVICE_NAME" --disable-hook-validation
 
         if [ $? -eq 0 ]; then
             log_message "INFO" "Certificate renewed successfully and $SWARM_SERVICE_NAME service updated."
